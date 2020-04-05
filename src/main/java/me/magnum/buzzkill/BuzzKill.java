27c806bd4ee1b2616dbbe6d6700abb6b240af89c
Richard Simpson <magnum1997@gmail.com>
2020-04-05 01:47:17 -0700

    Initial commit

---
+++ src/main/java/me/magnum/buzzkill/BuzzKill.java
@@ -0,0 +1,39 @@
+package me.magnum.buzzkill;
+
+import lombok.extern.java.Log;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Log
+public class BuzzKill extends JavaPlugin implements Listener {
+
+	//	private static BuzzKill plugin;
+
+	@Override
+	public void onEnable () {
+		//		BuzzKill plugin = this;
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@Override
+	public void onDisable () {
+		super.onDisable();
+	}
+
+	@EventHandler (priority = EventPriority.HIGHEST)
+	private void catchBees (CreatureSpawnEvent event) {
+		if (event.getEntity().getType().equals(EntityType.BEE)) {
+			String spawnReason = event.getSpawnReason().toString();
+			event.setCancelled(true);
+			log.info("Bee spawn prevented due to: " + spawnReason);
+			for (Player player : getServer().getOnlinePlayers()) {
+				player.sendMessage("[BuzzKill] Just prevented bee spawn - Reason: " + spawnReason);
+			}
+		}
+	}
+}
